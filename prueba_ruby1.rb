7250832ea0b69c1450b4f9de0feb6fd300f3cc31
Andrea Castro <ap.castrocastellon@gmail.com>
2019-05-14 11:56:36 -0400

    muestra promedios

--- prueba_ruby1.rb
+++ prueba_ruby1.rb
@@ -8,11 +8,28 @@ end
 #----------------SECTOR-METODOS-----------------------
 
 def corrige
-    #para corregir arreglo
+    alum = read_alum('alumnos.csv')
+    alum2 = alum.each do |arr|
+            arr.each do |item|
+                arr.delete("A") && arr.push("0")  if item == "A"            
+        end    
+    end 
 end
 
-def average
-    #para calcular promedio
+def average #para calcular promedio
+
+puts "--------------------------------------------"
+puts "Detalle de promedios:"
+    arr = corrige #cambia las a por ceros para poder sumar los subarreglos
+    arr.each do |item_line| #dentro del arreglo grande
+        acc = 0 #acumulador para la suma
+        item_line.each_with_index do |elem,index| #dentro del arreglo pequeño
+            acc += elem.to_f if index != 0 #sumale al acumulador los elemento como flotantes si son distintos de cero
+        end    
+        puts "\n El promedio de #{item_line[0]} es #{acc/((item_line.length)-1)}"
+    end
+puts "--------------------------------------------"
+
 end
 
 def absent
@@ -27,7 +44,7 @@ end
 
 condition = 0
 while condition != 4
-    puts "Elige una opcion entre 1 y 3, 4 para salir"
+    puts "\n Elige una opcion entre 1 y 3, 4 para salir"
     condition = gets.chomp.to_i
     
     case condition
