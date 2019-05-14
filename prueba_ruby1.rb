4e3eb068fe6a3197c1a0610a18eefae1cd3084fb
Andrea Castro <ap.castrocastellon@gmail.com>
2019-05-14 11:30:23 -0400

    esqueleto

--- prueba_ruby1.rb
+++ prueba_ruby1.rb
@@ -4,3 +4,42 @@ def read_alum(file_name)
     file.close
     alum
 end
+
+#----------------SECTOR-METODOS-----------------------
+
+def corrige
+    #para corregir arreglo
+end
+
+def average
+    #para calcular promedio
+end
+
+def absent
+    #para identificar inasistencias
+end
+
+def approved
+    #para identificar aprovados con criterio de 5.0
+end
+
+#---------------SECTOR-MENU-----------------------------
+
+condition = 0
+while condition != 4
+    puts "Elige una opcion entre 1 y 3, 4 para salir"
+    condition = gets.chomp.to_i
+    
+    case condition
+    when 1 #saca promedio
+        average
+    when 2 #identifica ausentes
+        absent     
+    when 3 #identifica aprobados
+        approved  
+    when 4 #termina programa
+        puts "Adios"
+    else
+        puts "opcion invalida, intenta nuevamente"
+    end 
+end
