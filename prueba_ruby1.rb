8661d47fba8b1be4ed26937851ae57b2a94641e5
Andrea Castro <ap.castrocastellon@gmail.com>
2019-05-14 12:29:20 -0400

    agrega formato

--- prueba_ruby1.rb
+++ prueba_ruby1.rb
@@ -6,7 +6,6 @@ def read_alum(file_name)
 end
 
 #----------------SECTOR-METODOS-----------------------
-
 def corrige
     alum = read_alum('alumnos.csv')
     alum2 = alum.each do |arr|
@@ -15,38 +14,47 @@ def corrige
         end    
     end 
 end
-
 def average #para calcular promedio
-
 puts "--------------------------------------------"
-puts "Detalle de promedios:"
+puts "DETALLE DE PROMEDIOS:"
     arr = corrige #cambia las a por ceros para poder sumar los subarreglos
     arr.each do |item_line| #dentro del arreglo grande
         acc = 0 #acumulador para la suma
         item_line.each_with_index do |elem,index| #dentro del arreglo pequeño
             acc += elem.to_f if index != 0 #sumale al acumulador los elemento como flotantes si son distintos de cero
         end    
-        puts "\n El promedio de #{item_line[0]} es #{acc/((item_line.length)-1)}"
+        puts "El promedio de #{item_line[0]} es #{acc/((item_line.length)-1)}"
     end
 puts "--------------------------------------------"
-
 end
-
-def absent
-    #para identificar inasistencias
+def absent #para identificar inasistencias
+    puts "--------------------------------------------"
+    puts "INASISTENCIAS"
+    arr = corrige
+    arr.each do |item_line| #dentro del arreglo grande
+        puts "Las inasistencias de #{item_line[0]} son : #{item_line.count("0")}" #cuenta las veces que es cero
+    end
+    puts "--------------------------------------------"
 end
-
-def approved
-    #para identificar aprovados con criterio de 5.0
+def approved (criteria = 5.0) #para identificar aprobados con criterio estatico de 5.0
+    arr = corrige
+    puts "--------------------------------------------"
+    puts "APRUEBA, considerando criterio de aprobacion >= 5.0: "
+    arr.each do |item_line|
+        acc = 0 #acumulador para la suma para el promedio
+        item_line.each_with_index do |elem,index| #dentro del arreglo pequeño
+            acc += elem.to_f if index != 0 #suma los elementos para luego sacar promedio
+        end
+        averag = acc/((item_line.length)-1) #nuevo promedio
+        puts "#{item_line[0]} ya que su promedio es #{averag}" if averag >= criteria 
+    end
+    puts "--------------------------------------------"
 end
-
 #---------------SECTOR-MENU-----------------------------
-
 condition = 0
 while condition != 4
-    puts "\n Elige una opcion entre 1 y 3, 4 para salir"
+    puts "\nEscribe\n 1 para ver DETALLE DE PROMEDIOS\n 2 para ver INASISTENCIAS\n 3 para ver los APROBADOS\n 4 SALIR"
     condition = gets.chomp.to_i
-    
     case condition
     when 1 #saca promedio
         average
@@ -57,6 +65,8 @@ while condition != 4
     when 4 #termina programa
         puts "Adios"
     else
+        puts "-----------------------------------"
         puts "opcion invalida, intenta nuevamente"
+        puts "-----------------------------------"
     end 
 end
